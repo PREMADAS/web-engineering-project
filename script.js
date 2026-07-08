b9ea9059bfdbb20d79fe9aaed92c859959137486
Durjoy Barua <durjoybarua504@gmail.com>
2026-07-08 11:26:51 +0600

    tic_tac_toe

--- script.js
+++ script.js
@@ -1,88 +1,100 @@
-const playerHand = document.getElementById("playerHand");
-const computerHand = document.getElementById("computerHand");
-const resultText = document.getElementById("result");
+const boxes = document.querySelectorAll(".square");
+const status = document.getElementById("status");
+const restart = document.getElementById("restart");
+
+const popup = document.getElementById("popup");
+const popupText = document.getElementById("popup-text");
+const closeBtn = document.getElementById("close-btn");
 
 
-const playerScoreText = document.getElementById("playerScore");
-const computerScoreText = document.getElementById("computerScore");
+let playerX = true;
+let gameRunning = true;
 
+const winPatterns = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 
-let playerScore = 0;
-let computerScore = 0;
-
-const icons = {
-    rock: "fa-hand-back-fist",
-    paper: "fa-hand",
-    scissors: "fa-hand-scissors"
-};
-
-function playGame(playerChoice) {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    const computerChoice = choices[randomIndex];
-
-    // Reset hands to fist before shaking
-    playerHand.innerHTML = `<i class="fa-regular fa-hand-back-fist rotate-90"></i>`;
-    computerHand.innerHTML = `<i class="fa-regular fa-hand-back-fist rotate-270"></i>`;
-
-    // Add shaking animation
-    playerHand.classList.add("shake-left");
-    computerHand.classList.add("shake-right");
-
-    resultText.innerText = "Playing...";
-
-    setTimeout(() => {
-        // Stop animation
-        playerHand.classList.remove("shake-left");
-        computerHand.classList.remove("shake-right");
-
-
-
-        let playerRotationClass = "rotate-90";
-        if (playerChoice === "scissors") {
-            playerRotationClass = "scale-x-[-1]"; // 
-        }
-
-
-        let computerRotationClass = "rotate-270";
-        if (computerChoice === "scissors") {
-            computerRotationClass = "scale-y-[-1]";
-        }
-
-
-        playerHand.innerHTML = `<i class="fa-regular ${playerRotationClass} ${icons[playerChoice]}"></i>`;
-        computerHand.innerHTML = `<i class="fa-regular ${computerRotationClass} ${icons[computerChoice]}"></i>`;
-
-        // Result Logic & Score Update
-        if (playerChoice === computerChoice) {
-            resultText.innerText = "Draw!! 🤝";
-        }
-        else if (
-            (playerChoice === "rock" && computerChoice === "scissors") ||
-            (playerChoice === "paper" && computerChoice === "rock") ||
-            (playerChoice === "scissors" && computerChoice === "paper")
-        ) {
-            resultText.innerText = "You Win 🎉";
-            playerScore++;
-            playerScoreText.innerText = playerScore;
+boxes.forEach((square) => {
+    square.addEventListener("click", () => {
+        console.log("button is pressed");
+        if (playerX === true) {
+            square.innerText = "X";
+            playerX = false;
+            status.innerText = "Player 0's Turn"
         }
         else {
-            resultText.innerText = "Computer Wins 😢";
-            computerScore++;
-            computerScoreText.innerText = computerScore;
+            square.innerText = "0";
+            playerX = true;
+            status.innerText = "Player X's Turn"
         }
-    }, 2000);
-}
+        square.disabled = true;
 
+        checkWinner();
+        checkDraw();
+    });
+});
 
-function restartGame() {
-    playerScore = 0;
-    computerScore = 0;
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
 
-    playerScoreText.innerText = "0";
-    computerScoreText.innerText = "0";
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("Winner is player ", pos1Val);
+                popupText.innerText = "Winner is Player " + pos1Val;
+                popup.classList.remove("hide");
+                status.innerText = "Game Over";
+                disableBoxes();
+                break;
 
-    playerHand.innerHTML = `<i class="fa-regular fa-hand-back-fist rotate-90"></i>`;
-    computerHand.innerHTML = `<i class="fa-regular fa-hand-back-fist rotate-270"></i>`;
-    resultText.innerText = "Let's Play!! ";
-}
+            }
+
+        }
+
+    }
+};
+
+const checkDraw = () => {
+    let allFilled = true;
+
+    boxes.forEach((square) => {
+        if (square.innerText === "") {
+            allFilled = false;
+        }
+    });
+
+    if (allFilled === true) {
+        popupText.innerText = "Match Draw";
+        popup.classList.remove("hide");
+        status.innerText = "Game Draw";
+        disableBoxes();
+    }
+};
+
+const disableBoxes = () => {
+    for (let square of boxes) {
+        square.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for (let square of boxes) {
+        square.disabled = false;
+        square.innerText = "";
+        status.innerText = "Player X's Turn";
+    }
+};
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.add("hide");
+    enableBoxes();
+});
